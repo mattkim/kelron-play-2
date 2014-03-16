a415bb1abd61a51e7661cd80a514003385786846
Kara Fong <kara@Karas-MacBook-Pro.local>
2014-03-16 10:49:55 -0700

    Testing photo 1 exp

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,13 +4,18 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+  var photo1: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-frc3/t1.0-9/10001387_10152330307280844_851329358_n.jpg"
 
   def index = Action {
-    Ok(views.html.index(""))
+    Ok(views.html.index(photo1))
   }
   
   def sendRSVP(){
     println("testt")
   }
 
+}
+
+object Images {
+  var photo1 : String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-frc3/t1.0-9/10001387_10152330307280844_851329358_n.jpg"
 }
