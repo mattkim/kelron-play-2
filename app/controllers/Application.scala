a4475c9bf2b0c19986ae7f218cdddf30b4e7d7bb
Kara Fong <kara@Karas-MacBook-Pro.local>
2014-03-16 11:02:02 -0700

    Swapping out pics 1 4 6 8

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,17 +5,20 @@ import play.api.mvc._
 
 object Application extends Controller {
   var photo1: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-frc3/t1.0-9/10001387_10152330307280844_851329358_n.jpg"
+  var photo2: String = ""
+  var photo3: String = ""
+  var photo4: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-ash3/t1.0-9/1794515_10152330307315844_778753097_n.jpg"
+  var photo5: String = ""
+  var photo6: String = "https://scontent-b-sea.xx.fbcdn.net/hphotos-ash3/t1.0-9/1932344_10152330307320844_1510788078_n.jpg"
+  var photo7: String = ""
+  var photo8: String = "https://scontent-b-sea.xx.fbcdn.net/hphotos-frc1/t1.0-9/1920613_10152330307350844_1272257753_n.jpg"
 
   def index = Action {
-    Ok(views.html.index(photo1))
+    Ok(views.html.index(photo1,photo2,photo3,photo4,photo5,photo6,photo7,photo8))
   }
   
   def sendRSVP(){
     println("testt")
   }
 
-}
-
-object Images {
-  var photo1 : String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-frc3/t1.0-9/10001387_10152330307280844_851329358_n.jpg"
 }
