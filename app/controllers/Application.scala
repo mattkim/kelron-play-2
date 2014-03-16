6f5711295eea77f4842042b82d07295b72a585b4
Kara Fong <kara@Karas-MacBook-Pro.local>
2014-03-16 14:50:45 -0700

    Adding new Google Maps features

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,19 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+
+
+object Images {
+  var photo1: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-frc3/t1.0-9/10001387_10152330307280844_851329358_n.jpg"
+  var photo2: String = ""
+  var photo3: String = ""
+  var photo4: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-ash3/t1.0-9/1794515_10152330307315844_778753097_n.jpg"
+  var photo5: String = ""
+  var photo6: String = "https://scontent-b-sea.xx.fbcdn.net/hphotos-ash3/t1.0-9/1932344_10152330307320844_1510788078_n.jpg"
+  var photo7: String = ""
+  var photo8: String = "https://scontent-b-sea.xx.fbcdn.net/hphotos-frc1/t1.0-9/1920613_10152330307350844_1272257753_n.jpg"
+}
+
 object Application extends Controller {
 
   def index = Action {
@@ -14,15 +27,4 @@ object Application extends Controller {
     println("testt")
   }
 
-}
-
-object Images {
-  var photo1: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-frc3/t1.0-9/10001387_10152330307280844_851329358_n.jpg"
-  var photo2: String = ""
-  var photo3: String = ""
-  var photo4: String = "https://scontent-a-sea.xx.fbcdn.net/hphotos-ash3/t1.0-9/1794515_10152330307315844_778753097_n.jpg"
-  var photo5: String = ""
-  var photo6: String = "https://scontent-b-sea.xx.fbcdn.net/hphotos-ash3/t1.0-9/1932344_10152330307320844_1510788078_n.jpg"
-  var photo7: String = ""
-  var photo8: String = "https://scontent-b-sea.xx.fbcdn.net/hphotos-frc1/t1.0-9/1920613_10152330307350844_1272257753_n.jpg"
 }
